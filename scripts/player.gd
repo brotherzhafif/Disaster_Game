67853ce6b7d7d5cbf511928d007440d416c0852d
BrotherZhafif <bangz1504@gmail.com>
2024-08-22 00:41:00 +0700

    Nambah Basic Player Movement dan Animasi

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,30 +1,49 @@
 extends CharacterBody3D
 
+@onready var camera_mount = $camera_mount
+@onready var animation_player = $visuals/mixamo_base/AnimationPlayer
+@onready var visuals = $visuals
 
-const SPEED = 5.0
+const SPEED = 2.25
 const JUMP_VELOCITY = 4.5
+
+@export var sens_horizontal = 0.5
+@export var sens_vertical = 0.25
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		$camera_mount.rotate_x(deg_to_rad(event.relative.y * sens_vertical))
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("leftward", "rightward", "forward", "backward")
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		if animation_player.current_animation != "walking":
+			animation_player.play("walking")
+		
+		visuals.look_at(position + direction)
+			
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
