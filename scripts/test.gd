9c63820b484d2845beddb38217e0eb95266f6f19
BrotherZhafif <bangz1504@gmail.com>
2024-08-21 19:43:31 +0700

    Menambahkan Folder Scenes dan Scripts biar Rapi

---
+++ scripts/test.gd
@@ -0,0 +1,9 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Hello World")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
