c86856cf2e34f73fa1a4bf5554c962107b280ca8
BrotherZhafif <bangz1504@gmail.com>
2024-08-23 23:49:41 +0700

    Memperbaiki Asset Model dan Jadikan Modelnya Mesh Library

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,22 +5,15 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-
-
-
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
-
 func _on_credit_pressed():
 	pass # Replace with function body.
-
 
 func _on_exit_pressed():
 	get_tree().quit()
